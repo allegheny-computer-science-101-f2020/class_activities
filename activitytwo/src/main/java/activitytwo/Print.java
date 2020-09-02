65928adec0af3397ff11a71a74260b7e43e7f689
Janyl Jumadinova <jjumadinova@allegheny.edu>
2020-09-01 22:26:25 -0400

    Modified program template

--- activitytwo/src/main/java/activitytwo/Print.java
+++ activitytwo/src/main/java/activitytwo/Print.java
@@ -1,21 +1,3 @@
 package activitytwo;
 
 import java.util.Scanner;
-
-/** PrintExample program in Java.
-* This program shows examples of String
-* concatenation and escape sequence usage.
-* @author Janyl Jumadinova
-*/
-
-public class Print {
-
-  /** main method containing print statements
-  * to demonstrate String printing and String
-  * concatenation.
-  */
-  public static void main(String [] args) {
-    System.out.println("Hello World!");
-    System.out.print("Be Brave!");
-  }
-}
